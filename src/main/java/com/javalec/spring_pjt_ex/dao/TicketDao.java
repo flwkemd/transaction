281290df8257aa3b3d2e75b17e6504b94743b76a
flwkemd <flwkemd@naver.com>
2017-10-23 01:59:20 +0200

    implemented transaction

--- src/main/java/com/javalec/spring_pjt_ex/dao/TicketDao.java
+++ src/main/java/com/javalec/spring_pjt_ex/dao/TicketDao.java
@@ -6,15 +6,25 @@ import java.sql.SQLException;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import com.javalec.spring_pjt_ex.dto.TicketDto;
 
 public class TicketDao {
 
 	JdbcTemplate template;
+	PlatformTransactionManager transactionManager;
+	
 	
 	public void setTemplate(JdbcTemplate template) {
 		this.template = template;
+	}
+
+	public void setTransactionManager(PlatformTransactionManager transactionManager) {
+		this.transactionManager = transactionManager;
 	}
 	
 	public TicketDao() {
@@ -26,33 +36,45 @@ public class TicketDao {
 		System.out.println("dto.getConsumerId() : " + dto.getConsumerId());
 		System.out.println("dto.getAmount() : " + dto.getAmount());
 		
-		template.update(new PreparedStatementCreator() {
-			
-			@Override
-			public PreparedStatement createPreparedStatement(Connection con)
-					throws SQLException {
-				String query = "insert into card (consumerId, amount) values (?, ?)";
-				PreparedStatement pstmt = con.prepareStatement(query);
-				pstmt.setString(1, dto.getConsumerId());
-				pstmt.setString(2, dto.getAmount());
-				
-				return pstmt;
-			}
-		});
+		TransactionDefinition definition = new DefaultTransactionDefinition();
+		TransactionStatus status = transactionManager.getTransaction(definition);
 		
-		template.update(new PreparedStatementCreator() {
-			
-			@Override
-			public PreparedStatement createPreparedStatement(Connection con)
-					throws SQLException {
-				String query = "insert into ticket (consumerId, countnum) values (?, ?)";
-				PreparedStatement pstmt = con.prepareStatement(query);
-				pstmt.setString(1, dto.getConsumerId());
-				pstmt.setString(2, dto.getAmount());
+		try {
+		
+			template.update(new PreparedStatementCreator() {
 				
-				return pstmt;
-			}
-		});
+				@Override
+				public PreparedStatement createPreparedStatement(Connection con)
+						throws SQLException {
+					String query = "insert into card (consumerId, amount) values (?, ?)";
+					PreparedStatement pstmt = con.prepareStatement(query);
+					pstmt.setString(1, dto.getConsumerId());
+					pstmt.setString(2, dto.getAmount());
+					
+					return pstmt;
+				}
+			});
+			
+			template.update(new PreparedStatementCreator() {
+				
+				@Override
+				public PreparedStatement createPreparedStatement(Connection con)
+						throws SQLException {
+					String query = "insert into ticket (consumerId, countnum) values (?, ?)";
+					PreparedStatement pstmt = con.prepareStatement(query);
+					pstmt.setString(1, dto.getConsumerId());
+					pstmt.setString(2, dto.getAmount());
+					
+					return pstmt;
+				}
+			});
+			
+			transactionManager.commit(status);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			transactionManager.rollback(status);
+		}
 	}
 
 	
